2dde4583ab393bf512df7aadcbc478be54e41f23
KUDOAYANO <ayanokudo1211@gmail.com>
2020-11-06 12:05:24 +0900

    【更新】　ツールの更新

--- project/tool/c++ひな形/floor.h
+++ project/tool/c++ひな形/floor.h
@@ -16,6 +16,15 @@
 class CFloor : public CModel
 {
 public:
+    //床のテクスチャの種類
+    typedef enum
+    {
+        FLOOR_FLOORING = 0,	//フローリング
+        FLOOR_MAT,			//マット
+        FLOOR_KITCHEN,		//キッチン
+        FLOOR_MAX,
+    }FLOOR;
+
     // メンバ関数
     CFloor();
     ~CFloor();
@@ -28,11 +37,16 @@ public:
     void Update(void);
     void Draw(void);
 
+    // タイプのセッタ/ゲッタ
+    void SetType(FLOOR type) { m_Type = type; }
+    FLOOR GetType(void) { return m_Type; }
+
 private:
     // メンバ変数
     static LPD3DXMESH           m_pMeshModel;       //メッシュ情報へのポインタ
     static LPD3DXBUFFER         m_pBuffMatModel;    //マテリアル情報へのポインタ
     static DWORD                m_nNumMatModel;     //マテリアル情報の数
     static LPDIRECT3DTEXTURE9   m_apTexture;        // テクスチャ
+    FLOOR m_Type;
 };
 #endif      // _FLOOR_H_

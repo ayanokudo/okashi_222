b1652563843f32a5ea9fda87ab6d004a28b44456
toshikitsutsui0909 <toshiki.0909.tsutsui@gmail.com>
2020-11-09 13:34:51 +0900

    更新

--- project/12on-line/c++ひな形/boss.cpp
+++ project/12on-line/c++ひな形/boss.cpp
@@ -18,6 +18,7 @@
 #include "player.h"
 #include "bullet.h"
 #include "motion.h"
+#include "score.h"
 #include "debug_log.h"
 
 //*****************************
@@ -288,6 +289,7 @@ void CBoss::Hit(int nDamage)
 
 	if (m_nLife <= 0)
 	{
+		CScore::AddScore(15000);
 		Uninit();
 		return;
 	}
